5031a11da51ae0ffd92fd03057380a9852584b93
W1ll1a <71302611+W1ll1a@users.noreply.github.com>
2020-10-12 15:56:45 -0600

    Update Triangulo.cpp

--- Triangulo.cpp
+++ Triangulo.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <cstdio>
-
+//Function to calculate fibonnachi.
 unsigned fibonacchi(unsigned posicion)
 {
+    //fibonp1 stands for fibonnachi position 1, same with fibonp2.
     unsigned fibonP1 = 0;
     unsigned fibonP2 = 1;
     unsigned fibonResultante = 1;
@@ -16,6 +17,7 @@ unsigned fibonacchi(unsigned posicion)
     }
     else
     {
+        //for loop to calculate all positions the user wants.
         for (unsigned F = 3; F <= posicion; F++)
         {
             fibonResultante = fibonP1 + fibonP2;
@@ -27,16 +29,21 @@ unsigned fibonacchi(unsigned posicion)
 }
 unsigned Hosoya(unsigned Alto, unsigned Largo)
 {
+    //Hosoyas formula is H=fibonachi(i+1)*fibonacchi(n-i+1)
     unsigned Hosoyas = fibonacchi(Largo+1) * fibonacchi(Alto - Largo+2);
     return Hosoyas;
 }
+//function to calculate position.
+
 void triangulo(unsigned nivel)
 {
+    //loop to make the triangle.
     for(unsigned alto = 1; alto <= nivel; alto++) {
         for(unsigned space=nivel-alto;space>0;space--){
             printf("    ");
         }
         for (unsigned largo = 1; largo <= alto; largo++) {
+            //calling hosoyas fuction, the numbers will appear in a triangle.
             printf("%u      ", Hosoya(alto,largo));
         }
         printf("\n");
